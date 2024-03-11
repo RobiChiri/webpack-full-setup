d5a53d54abc9f7f0cd9295aa7ea06f4901241f39
Roberto <robertochiriac8@gmail.com>
2024-03-11 09:32:32 +0100

    Added McLaren images and updated webpack config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import "../styles/index.scss";
 import F1 from "./F1";
+import mclaren from "../images/2954472-60645375-1600-900.jpg";
+import mclaren_logo from "../images/mclaren-svgrepo-com.svg";
 
 const App = () => {
   return (
@@ -9,8 +11,11 @@ const App = () => {
         <section>
           <h1>REACT</h1>
         </section>
-          </main>
-          <F1 />
+        <img src={mclaren} alt="mclaren" width="250px" />
+        <img src={mclaren_logo} alt="mclaren" width="150px" />
+
+        <F1 />
+      </main>
     </>
   );
 };
